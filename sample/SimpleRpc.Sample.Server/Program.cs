2822437298488c0403740437f4198aaa05acfc77
Daniil Sokolyuk <d.sokolyuk@rambler-co.ru>
2019-11-03 23:11:51 +0300

    update

--- sample/SimpleRpc.Sample.Server/Program.cs
+++ sample/SimpleRpc.Sample.Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace SimpleRpc.Sample.Server
 {
@@ -7,12 +8,14 @@ namespace SimpleRpc.Sample.Server
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
